7ce355909c42c7e86f118a1b268f3f619ea3d961
Ned Batchelder <ned@nedbatchelder.com>
2015-04-02 20:20:27 -0400

    Don't show the key on the big screen

--- meetup.py
+++ meetup.py
@@ -1,11 +1,13 @@
 """Rough and ready meetup.com api wrapper."""
 
 import json
+import re
 import urllib, urllib2
 
 from keys import API_KEY
 
 DEBUG = 1
+SECRET = 1
 
 def meetup_call(slug, **kwargs):
     kwargs['key'] = API_KEY
@@ -15,7 +17,10 @@ def meetup_call(slug, **kwargs):
 
 def meetup_url_call(url):
     if DEBUG > 0:
-        print "opening %r" % url
+        show_url = url
+        if SECRET:
+            show_url = re.sub(r"key=[0-9a-fA-F]{30}", "key=xyzzy", show_url)
+        print "opening %r" % show_url
 
     headers = { 'Accept-Charset': 'utf-8' }
     req = urllib2.Request(url, None, headers)
